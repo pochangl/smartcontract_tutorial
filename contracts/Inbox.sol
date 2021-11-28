4f70a4fe0a1305c5e94caff7b7a42e6b56ed90fd
Pochang Lee <pochanglee2008@gmail.com>
2021-11-28 20:17:56 +0800

    compile contract

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -3,18 +3,11 @@ pragma solidity ^0.4.17;
 contract Inbox {
     string public message;
 
-    constructor(string initialMessage) public {
+    function Inbox(string initialMessage) public {
         message = initialMessage;
     }
 
     function setMessage(string newMessage) public {
         message = newMessage;
-    }
-
-    function doMath(int256 a, int256 b) {
-        a + b;
-        a - b;
-        a * b;
-        a == 0;
     }
 }
